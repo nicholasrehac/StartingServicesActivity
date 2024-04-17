29363d5058291c594def5ffa8f6e7486d5fb51fa
Nicholas Rehac <tup88289@temple.edu>
2024-04-17 15:41:49 -0400

    TimerService started with parameters passed via intent

--- app/src/main/java/edu/temple/startingservicesactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/startingservicesactivity/MainActivity.kt
@@ -1,11 +1,28 @@
 package edu.temple.startingservicesactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    val timerEditText: EditText by lazy {
+        findViewById(R.id.timerEditText)
+    }
+    val startButton: Button by lazy {
+        findViewById(R.id.startButton)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener {
+            startService(
+                Intent(this, TimerService::class.java).apply {
+                    putExtra(TimerService.COUNTDOWN_KEY, timerEditText.text.toString().toInt())
+                }
+            )
+        }
     }
 }
--- app/src/main/java/edu/temple/startingservicesactivity/TimerService.kt
+++ app/src/main/java/edu/temple/startingservicesactivity/TimerService.kt
@@ -3,10 +3,26 @@ package edu.temple.startingservicesactivity
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerService : Service() {
 
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var countdown = intent?.getIntExtra(COUNTDOWN_KEY, 0) ?: 0
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    companion object {
+        const val COUNTDOWN_KEY = "countdown"
     }
 }
